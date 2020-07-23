8c4b460800af422698fc6ba2b10fe252c906efc9
Michael Barz <mb@barz.de>
2020-07-23 15:49:12 +0200

    improved downsampling processor

--- multisensor_pipeline/modules/signal/sampling.py
+++ multisensor_pipeline/modules/signal/sampling.py
@@ -1,6 +1,7 @@
 from multisensor_pipeline import BaseProcessor
 from multisensor_pipeline.utils.dataframe import MSPDataFrame
 import logging
+import numpy as np
 
 
 logger = logging.getLogger(__name__)
@@ -8,12 +9,80 @@ logger = logging.getLogger(__name__)
 
 class DownsamplingProcessor(BaseProcessor):
 
-    _dtype_in = None
-    _sampling_rate = None
-    _period_time = None
+    class SampleHistory:
 
-    _last_sample_sent = {}
-    _last_sample_received = {}
+        def __init__(self, dtype, fps_out):
+            self.dtype = dtype
+            self.target_fps = fps_out
+            self.target_period_time = 1. / self.target_fps
+            self.last_sent = None
+            self.samples = []
+            self._timestamps_out = []  # for fps estimation
+
+        def add(self, sample):
+            self.samples.append(sample)
+
+        def _update_and_get_last_sample(self, sample_id):
+            self.last_sent = self.samples[sample_id]
+            # keep the last 5 timestamps for estimating actual frame-rate
+            self._timestamps_out.append(self.last_sent.timestamp)
+            if len(self._timestamps_out) > 10:
+                self._timestamps_out = self._timestamps_out[1:]
+            # logger.info(f"{round(self.fps_out, 3)} fps (out)")
+            # keep samples that arrived after the sent one
+            self.samples = self.samples[sample_id + 1:]
+            return self.last_sent
+
+        def get_send_sample(self):
+            assert len(self.samples) > 0, "there was no sample yet"
+
+            # if nothing was sent before take first=last sample and reset the list
+            if self.last_sent is None:
+                assert len(self.samples) == 1, "if nothing was sent, there should be only one sample in the list"
+                return self._update_and_get_last_sample(0)
+            else:
+                # check whether current sample shall be sent
+                current_time = self.last_received.timestamp
+                last_sent_time = self.last_sent.timestamp
+                current_period_time = current_time - last_sent_time
+                if current_period_time >= self.target_period_time:  # - self.current_delay:
+                    # send sample that is closest to period time (+/- delay to keep up with target frame-rate)
+                    recent_timestamps = np.array([s.timestamp for s in self.samples])
+                    recent_time_diffs = np.fabs(recent_timestamps - last_sent_time - self.target_period_time)
+                    sample_id = np.argmin(recent_time_diffs)
+                    return self._update_and_get_last_sample(sample_id)
+
+            return None
+
+        @property
+        def current_delay(self):
+            """returns the deviation from the targeted period time"""
+            return self.period_time_out - self.target_period_time
+
+        @property
+        def fps_out(self):
+            return 1. / self.period_time_out
+
+        @property
+        def period_time_out(self):
+            if len(self._timestamps_out) < 3:
+                return self.target_period_time
+            else:
+                period_time_estimate = (self._timestamps_out[-1] - self._timestamps_out[0]) / \
+                                       (len(self._timestamps_out) - 1)
+                return period_time_estimate
+
+        @property
+        def last_received(self):
+            return self.samples[-1]
+
+        @property
+        def fps_in(self):
+            return 1. / self.period_time_in
+
+        @property
+        def period_time_in(self):
+            return (self.samples[-1].timestamp - self.samples[0].timestamp) / len(self.samples)
 
     def __init__(self, dtype=None, sampling_rate=5):
         """
@@ -27,25 +96,25 @@ class DownsamplingProcessor(BaseProcessor):
         self._sampling_rate = sampling_rate
         self._period_time = 1. / sampling_rate
 
-    def _handle_dataframe(self, dtype, dataframe: MSPDataFrame):
-        if dtype not in self._last_sample_sent:
-            self._notify_all(dtype, dataframe, suffix=f"{self._sampling_rate}Hz")
-            self._last_sample_sent[dtype] = dataframe
-            self._last_sample_received[dtype] = dataframe
-            return
+        self._sample_hist = dict()
+        self._last_sent = dict()
+        self._last_received = dict()
 
-        timestamp = dataframe.timestamp
-        last_sent_period_time = timestamp - self._last_sample_sent[dtype].timestamp
-        last_received_period_time = timestamp - self._last_sample_received[dtype].timestamp
-        if last_sent_period_time >= self._period_time:
-            # send the sample that is closest to the optimal sampling timestamp (= last_timestamp + period_time)
-            if abs(last_sent_period_time - self._period_time) <= abs(last_received_period_time - self._period_time):
-                self._notify_all(dtype, dataframe, suffix=f"{self._sampling_rate}Hz")
-                self._last_sample_sent[dtype] = dataframe
-            else:
-                self._notify_all(dtype, self._last_sample_received[dtype], suffix=f"{self._sampling_rate}Hz")
-                self._last_sample_sent[dtype] = self._last_sample_received[dtype]
-        self._last_sample_received[dtype] = dataframe
+    def _send_sample(self, dtype, dataframe):
+        self._notify_all(dtype, dataframe, suffix=f"{self._sampling_rate}Hz")
+        self._last_sent[dtype] = dataframe
+
+    def _get_history(self, dtype) -> SampleHistory:
+        if dtype not in self._sample_hist:
+            self._sample_hist[dtype] = self.SampleHistory(dtype, fps_out=self._sampling_rate)
+        return self._sample_hist[dtype]
+
+    def _handle_dataframe(self, dtype, dataframe: MSPDataFrame):
+        hist = self._get_history(dtype)
+        hist.add(dataframe)
+        send_sample = hist.get_send_sample()
+        if send_sample is not None:
+            self._notify_all(dtype, send_sample, suffix=f"{self._sampling_rate}Hz")
 
     def _update_loop(self):
         while self._active:
