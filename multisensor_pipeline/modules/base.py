3b082b8fac19236d10d2dc2f6721e373b0502fd2
Michael Barz <mb@barz.de>
2021-03-17 17:14:46 +0100

    added a worker function that calls _update (#4); currently the worker function does not add any new functionality.

--- multisensor_pipeline/modules/base.py
+++ multisensor_pipeline/modules/base.py
@@ -1,62 +1,51 @@
 from abc import ABC
-from multisensor_pipeline.utils.dataframe import TypeInfo, TypeMismatchException
 from threading import Thread
 from queue import Queue
-from ..utils.dataframe import MSPDataFrame
+from ..utils.dataframe import MSPDataFrame, TypeInfo, TypeMismatchException
 import logging
 
 logger = logging.getLogger(__name__)
 
 
 class BaseModule(object):
-    """
-    Base class for all modules.
-    """
+    """ Base class for all modules. """
 
     def __init__(self):
-        self._thread = Thread(target=self._update)
+        self._thread = Thread(target=self._worker)
         self._active = False
 
     def get_name(self):
-        """
-        Returns: Name of the actual subclass
-        """
+        """ Returns the name of the actual subclass """
         return self.__class__.__name__
 
     def start(self):
-        """
-        Starts the module.
-        """
+        """ Starts the module. """
         logger.debug("starting: {}".format(self.get_name()))
         self._active = True
         self._start()
         self._thread.start()
 
     def _start(self):
-        """
-        Custom initialization
-        """
+        """ Custom initialization """
         pass
 
+    def _worker(self):
+        """ Main worker function (async) """
+        self._update()
+
     def _update(self):
-        """
-        Main worker function (async)
-        """
+        """ Custom update routine. """
         raise NotImplementedError()
 
     def stop(self):
-        """
-        Stops the module.
-        """
+        """ Stops the module. """
         logger.debug("stopping: {}".format(self.get_name()))
         self._active = False
         self._thread.join()
         self._stop()
 
     def _stop(self):
-        """
-        Custom clean-up
-        """
+        """ Custom clean-up """
         pass
 
 
@@ -64,9 +53,7 @@ class BaseSink(BaseModule, ABC):
     """ Base class for data sinks. """
 
     def __init__(self):
-        """
-        Initializes the worker thread and a queue that will receive new samples from sources.
-        """
+        """ Initializes the worker thread and a queue that will receive new samples from sources. """
         super().__init__()
         self._source_queue = Queue()
         self._consumes = None
@@ -83,9 +70,7 @@ class BaseSink(BaseModule, ABC):
 
 
 class BaseSource(BaseModule, ABC):
-    """
-    Base class for data sources.
-    """
+    """ Base class for data sources. """
 
     def __init__(self):
         """
@@ -122,9 +107,11 @@ class BaseSource(BaseModule, ABC):
             data: the actual payload as an instance of MSPDataFrame
         """
 
+        # TODO: move dtype to MSPDataFrame
         if type(dtype) is not bytes:
             dtype = str(dtype).encode()
 
+        # TODO: enforce using MSPDataFrame instances
         if not isinstance(data, MSPDataFrame):
             data = MSPDataFrame(init_dict={"data": data})
 
