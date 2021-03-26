6844a446c2096f725305ccad9a523f327a68270a
Michael Barz <mb@barz.de>
2021-03-26 17:00:11 +0100

    updated network package #27

--- multisensor_pipeline/modules/network.py
+++ multisensor_pipeline/modules/network.py
@@ -19,11 +19,9 @@ class ZmqPublisher(BaseSink):
         self.socket = self.context.socket(zmq.PUB)
         self.socket.bind("{}://{}:{}".format(self.protocol, self.url, self.port))
 
-    def _update(self, frame=None):
-        while self._active:
-            dtype, data = self.get()
-            payload = (dtype, msgpack.packb(data, use_bin_type=True))
-            self.socket.send_multipart(payload)
+    def _update(self, frame: MSPDataFrame = None):
+        payload = (frame.topic.name, msgpack.packb(frame, use_bin_type=True))
+        self.socket.send_multipart(payload)
 
     def _stop(self):
         self.socket.close()
@@ -32,7 +30,7 @@ class ZmqPublisher(BaseSink):
 
 class ZmqSubscriber(BaseSource):
 
-    def __init__(self, source_filter='', protocol='tcp', url='127.0.0.1', port=5000):
+    def __init__(self, topic_filter='', protocol='tcp', url='127.0.0.1', port=5000):
         super(ZmqSubscriber, self).__init__()
 
         self.protocol = protocol
@@ -42,16 +40,13 @@ class ZmqSubscriber(BaseSource):
         self.socket = self.context.socket(zmq.SUB)
         self.socket.connect("{}://{}:{}".format(self.protocol, self.url, self.port))
 
-        self.source_filter = source_filter
+        self.source_filter = topic_filter
         self.socket.setsockopt_string(zmq.SUBSCRIBE, self.source_filter)
 
-    def _update(self, frame=None):
-        while self._active:
-            packet = self.socket.recv_multipart()
-            dtype = packet[0]
-            data = msgpack.unpackb(packet[1], raw=False)
-            data = MSPDataFrame(init_dict=data)
-            self._notify(dtype, data)
+    def _update(self) -> MSPDataFrame:
+        packet = self.socket.recv_multipart()
+        frame = msgpack.unpackb(packet[1], raw=False)
+        return frame
 
     def _stop(self):
         self.socket.close()
