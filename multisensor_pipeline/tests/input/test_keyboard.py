b3843c82bba97429b4dfbcf43ab77c50b9de0700
Bengt Lüers <bengt.lueers@gmail.com>
2021-05-14 19:32:34 +0200

    Avoid repetition by introducing a local name

--- multisensor_pipeline/tests/input/test_keyboard.py
+++ multisensor_pipeline/tests/input/test_keyboard.py
@@ -6,6 +6,7 @@ def test_simple_keyboard(xvfb):
     from multisensor_pipeline.modules.input.keyboard import Keyboard
     from multisensor_pipeline.pipeline.graph import GraphPipeline
 
+    # Mock
     # (1) define the modules
     source = Keyboard(press=True, release=True)
     sink = QueueSink()
@@ -17,10 +18,12 @@ def test_simple_keyboard(xvfb):
     # (3) ...and connect the modules
     pipeline.connect(source, sink)
 
+    # Test
     pipeline.start()
     sleep(.3)
     pipeline.stop()
 
+    # Assert
     # If we ever get here, we consider this test successful.
     assert True
 
@@ -32,8 +35,9 @@ def test_simulated_keyboard_input(xvfb):
 
     from pynput.keyboard import Controller, Key
 
-    keyboard = Controller()
+    # Mock
     # (1) define the modules
+    keyboard = Controller()
     source = Keyboard(press=True, release=True)
     sink = ListSink()
 
@@ -44,21 +48,20 @@ def test_simulated_keyboard_input(xvfb):
     # (3) ...and connect the modules
     pipeline.connect(source, sink)
 
+    # Test
     pipeline.start()
-
-    keyboard.press(Key.caps_lock)
-    keyboard.release(Key.caps_lock)
+    keyboard.press(Key.ctrl)
+    keyboard.release(Key.ctrl)
     sleep(.3)
     pipeline.stop()
+
+    # Assert
+    expected_events = 2
     if "darwin" in keyboard.__str__():
         # There seems te be a bug in pynput keyboard controller for MacOS:
         # Keypress is recognized as press and release
-        assert \
-            4 == len(sink.list), \
-            "number of keyboard interactions are not correctly recognized " + \
-            "or permission to simulate a keyboard is not given"
-    else:
-        assert \
-            2 == len(sink.list), \
-            "number of keyboard interactions are not correctly recognized " + \
-            "or permission to simulate a keyboard is not given"
+        expected_events *= 2
+    assert \
+        len(sink.list) == expected_events, \
+        "number of keyboard interactions are not correctly recognized " + \
+        "or permission to simulate a keyboard is not given"
