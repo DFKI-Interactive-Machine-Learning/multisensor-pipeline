c6801943a00b8ccc8b0e348a02ec825ef10cd1df
Bengt Lüers <bengt.lueers@gmail.com>
2021-05-18 16:39:49 +0200

    Relax more test conditions for macOS

--- multisensor_pipeline/tests/signal/test_sink_source_processor_sink.py
+++ multisensor_pipeline/tests/signal/test_sink_source_processor_sink.py
@@ -42,4 +42,4 @@ def test_down_sampling_processor_no_downsampling():
     assert not sink_1.queue.empty()
     assert 10 <= sink_1.queue.qsize()
     # No sink should not have received much more messages than the other
-    assert abs(sink_0.queue.qsize() - sink_1.queue.qsize()) <= 3
+    assert abs(sink_0.queue.qsize() - sink_1.queue.qsize()) <= 4
