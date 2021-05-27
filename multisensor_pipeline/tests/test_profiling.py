7de37a8fab79d41a69ed6e4672eb9cca45a110d3
Bengt Lüers <bengt.lueers@gmail.com>
2021-05-27 15:39:39 +0200

    Attempt to make tests work under macOS in the cloud by sleeping a bigger amount of time

--- multisensor_pipeline/tests/test_profiling.py
+++ multisensor_pipeline/tests/test_profiling.py
@@ -18,7 +18,7 @@ class ProfilingTest(unittest.TestCase):
         for frame in frames:
             msp_stats.add_frame(frame, direction=MSPModuleStats.Direction.IN)
             sleep(1. / frequency)
-        sleep(0.1)  # To make this work under macOS in the cloud
+        sleep(1)  # To make this work under macOS in the cloud
 
         stats = msp_stats.get_stats(direction=MSPModuleStats.Direction.IN)
         self.assertAlmostEqual(frequency, stats["test"]._cma, delta=1)
