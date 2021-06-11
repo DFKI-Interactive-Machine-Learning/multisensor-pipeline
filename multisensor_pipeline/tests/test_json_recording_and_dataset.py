0bc0318a3eee37c0476502386530fb7b1d769f3a
Bengt Lüers <bengt.lueers@gmail.com>
2021-06-11 15:42:40 +0200

    Increase alignment with main branch

--- multisensor_pipeline/tests/test_json_recording_and_dataset.py
+++ multisensor_pipeline/tests/test_json_recording_and_dataset.py
@@ -6,8 +6,7 @@ import numpy as np
 
 from multisensor_pipeline.modules.npy import RandomArraySource
 from multisensor_pipeline.tests.paths import DATA_PATH
-from multisensor_pipeline.modules.persistence.recording import \
-    JsonRecordingSink
+from multisensor_pipeline.modules.persistence.recording import JsonRecordingSink
 from multisensor_pipeline.modules.persistence.replay import JsonReplaySource
 from multisensor_pipeline.modules import ListSink
 from multisensor_pipeline.pipeline.graph import GraphPipeline
@@ -56,9 +55,7 @@ class JsonSerializationTest(unittest.TestCase):
         # dataset sources stop automatically
         json_pipeline.join()
 
-        assert \
-            [t.timestamp for t in rec_list.list] == \
-            [t.timestamp for t in json_list.list]
+        assert [t.timestamp for t in rec_list.list] == [t.timestamp for t in json_list.list]
 
         # --- check playback timing ---
         rec_timestamps = [t.timestamp for t in rec_list.list]
@@ -71,11 +68,7 @@ class JsonSerializationTest(unittest.TestCase):
         playback_frame_time = playback_time / (len(rec_timestamps) - 1)
         playback_fps = 1. / playback_frame_time
 
-        mean_frame_time_diff = \
-            np.fabs(
-                np.mean(np.diff(playback_timestamps)) -
-                np.mean(np.diff(rec_timestamps))
-            )
+        mean_frame_time_diff = np.fabs(np.mean(np.diff(playback_timestamps)) - np.mean(np.diff(rec_timestamps)))
         logging.info(
             f"Recording at {sampling_rate} Hz (actual: {rec_fps} Hz)\t"
             f"Playback ({playback_speed}x) at {playback_fps} Hz"
