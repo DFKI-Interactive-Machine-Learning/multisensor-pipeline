a16376b28638abaff4cebf6841475bfad16d2ead
Bengt Lüers <bengt.lueers@gmail.com>
2021-06-17 15:53:25 +0200

    Make comment fit in one line

--- multisensor_pipeline/tests/environment_properties.py
+++ multisensor_pipeline/tests/environment_properties.py
@@ -2,9 +2,7 @@ import os
 
 
 def is_running_in_ci() -> bool:
-    """
-    Returns whether or not this is running in continuous integration (CI).
-    """
+    """Return whether this is running in continuous integration (CI)."""
     this_is_running_in_ci: bool = bool(os.getenv('CI', default=str(False)))
 
     return this_is_running_in_ci
